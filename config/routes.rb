3e7009764fcf705be1296331940889faccfba9a6
Owen Baldwin <owenabaldwin@gmail.com>
2023-02-13 10:31:31 +0000

    membership page added with form

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get :about, to: 'pages#about'
   get :contact, to: 'pages#contact'
   get :studios, to: 'pages#studios'
+  get :membership, to: 'pages#membership'
 
 
 end
